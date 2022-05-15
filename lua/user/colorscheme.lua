ec300878d9a7916928f29f5cafeb19039e60161f
Rishabh <jharishabh2k3@gmail.com>
2022-05-15 15:31:11 +0530

    updated the plugins

--- lua/user/colorscheme.lua
+++ lua/user/colorscheme.lua
@@ -1,5 +1,5 @@
-local colorscheme = "tokyonight"
-vim.g.tokyonight_style = "night"
+local colorscheme = "catppuccin"
+--vim.g.tokyonight_style = "night"
 
 local status_ok, _ = pcall(vim.cmd, "colorscheme " .. colorscheme)
 if not status_ok then
