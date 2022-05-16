ed2dbfb250382a961c5387017ce8a0584fa1496e
Rishabh <jharishabh2k3@gmail.com>
2022-05-16 12:11:58 +0530

    updated the treesitter config

--- lua/user/treesitter.lua
+++ lua/user/treesitter.lua
@@ -1,3 +1,8 @@
+local status_ok, treesitter= pcall(require, "nvim-treesitter")
+if not status_ok then
+  return
+end
+
 local status_ok, configs = pcall(require, "nvim-treesitter.configs")
 if not status_ok then
   return
