36d9c99eedf2e2dae6e1ecb559e935c1a6327f4d
Rishabh <jharishabh2k3@gmail.com>
2022-05-16 11:50:17 +0530

    updated the config

--- lua/user/colorizer.lua
+++ lua/user/colorizer.lua
@@ -1,4 +1,5 @@
-local colorizer_status_ok, colorizer = pcall(require, "colorizer.setup()")
+local colorizer_status_ok, colorizer = pcall(require, "colorizer")
 if not colorizer_status_ok then
   return
 end
+require "colorizer".setup()
