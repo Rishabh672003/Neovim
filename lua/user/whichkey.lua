6a92c5bc908a9a681db88d76a9c772ec6d4a6ec2
Rishabh <jharishabh2k3@gmail.com>
2022-05-19 13:46:02 +0530

    updated the which-key

--- lua/user/whichkey.lua
+++ lua/user/whichkey.lua
@@ -94,9 +94,9 @@ local mappings = {
     "Find files",
   },
   ["F"] = { "<cmd>Telescope live_grep theme=ivy<cr>", "Find Text" },
-  ["P"] = { "<cmd>lua require('telescope').extensions.projects.projects()<cr>", "Projects" },
+  ["p"] = { "<cmd>lua require('telescope').extensions.projects.projects()<cr>", "Projects" },
 
-  p = {
+  P = {
     name = "Packer",
     c = { "<cmd>PackerCompile<cr>", "Compile" },
     i = { "<cmd>PackerInstall<cr>", "Install" },
