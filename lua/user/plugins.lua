fd70955df359f8df1faab0ab48b2cb8a856c67a2
Rishabh <jharishabh2k3@gmail.com>
2022-05-20 11:07:14 +0530

    removed telescope-media-files because it doesn't works on wayland

--- lua/user/plugins.lua
+++ lua/user/plugins.lua
@@ -75,7 +75,6 @@ return packer.startup(function(use)
 
   -- Telescope
   use "nvim-telescope/telescope.nvim"
-  use "nvim-telescope/telescope-media-files.nvim"
 
   --Treesitter
   use {
--- lua/user/telescope.lua
+++ lua/user/telescope.lua
@@ -3,7 +3,7 @@ if not status_ok then
   return
 end
 
-telescope.load_extension('media_files')
+--telescope.load_extension('media_files')
 
 local actions = require "telescope.actions"
 
@@ -88,13 +88,13 @@ telescope.setup {
     -- Now the picker_config_key will be applied every time you call this
     -- builtin picker
   },
-  extensions = {
-    media_files = {
+   extensions = {
+    -- media_files = {
         -- filetypes whitelist
         -- defaults to {"png", "jpg", "mp4", "webm", "pdf"}
-        filetypes = {"png", "webp", "jpg", "jpeg"},
-        find_cmd = "rg" -- find command (defaults to `fd`)
-      }
+        -- filetypes = {"png", "webp", "jpg", "jpeg"},
+        -- find_cmd = "rg" -- find command (defaults to `fd`)
+      -- }
     -- Your extension configuration goes here:
     -- extension_name = {
     --   extension_config_key = value,
