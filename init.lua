925cbd347cd8fff1464723c0490c25e0b0bd6367
Rishabh <jharishabh2k3@gmail.com>
2022-05-20 13:15:18 +0530

    updated the init.lua to use pcall to better handle errors

--- init.lua
+++ init.lua
@@ -1,22 +1,33 @@
-require "user.impatient"
-require "user.options"
-require "user.keymaps"
-require "user.plugins"
-require "user.colorscheme"
-require "user.cmp"
-require "user.lsp"
-require "user.telescope"
-require "user.treesitter"
-require "user.autopairs"
-require "user.comment"
-require "user.gitsigns"
-require "user.nvim-tree"
-require "user.bufferline"
-require "user.whichkey"
-require "user.toggleterm"
-require "user.project"
-require "user.indentline"
-require "user.whichkey"
-require "user.autocommands"
-require 'user.colorizer'
-require "user.lualine-themes.lualine1"
+local core_modules = {
+  "user.impatient",
+  "user.options",
+  "user.keymaps",
+  "user.plugins",
+  "user.colorscheme",
+  "user.cmp",
+  "user.lsp",
+  "user.telescope",
+  "user.treesitter",
+  "user.autopairs",
+  "user.comment",
+  "user.gitsigns",
+  "user.nvim-tree",
+  "user.bufferline",
+  "user.whichkey",
+  "user.toggleterm",
+  "user.project",
+  "user.indentline",
+  "user.whichkey",
+  "user.autocommands",
+  "user.colorizer",
+  "user.lualine-themes.lualine1",
+}
+
+-- Using pcall we can handle better any loading issues
+for _, module in ipairs(core_modules) do
+    local ok, err = pcall(require, module)
+    if not ok then
+      -- error("Error loading " .. module .. "\n\n" .. err)
+      return
+    end
+end
