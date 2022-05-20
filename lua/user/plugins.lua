34a928ba04785ed815c2c527a352f4bf2d5e36e6
Rishabh <jharishabh2k3@gmail.com>
2022-05-20 10:29:17 +0530

    corrected the plugins config

--- lua/user/plugins.lua
+++ lua/user/plugins.lua
@@ -123,7 +123,7 @@ return packer.startup(function(use)
       'kyazdani42/nvim-web-devicons', -- optional, for file icon
     },
     tag = 'nightly' -- optional, updated every week. (see issue #1193)
-}
+  }
   --use "nvim-neo-tree/neo-tree"
 
   --bufferline
