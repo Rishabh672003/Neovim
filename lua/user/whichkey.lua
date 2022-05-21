125512344c1c7f9f210c4423a3b923adb4e2da9f
Rishabh <jharishabh2k3@gmail.com>
2022-05-21 12:21:26 +0530

    corrected the formatting

--- lua/user/whichkey.lua
+++ lua/user/whichkey.lua
@@ -186,12 +186,13 @@ local mappings = {
 		name = "Terminal",
 		n = { "<cmd>lua _NODE_TOGGLE()<cr>", "Node" },
 		u = { "<cmd>lua _NCDU_TOGGLE()<cr>", "NCDU" },
-		t = { "<cmd>lua _HTOP_TOGGLE()<cr>", "Htop" },
+		H = { "<cmd>lua _HTOP_TOGGLE()<cr>", "Htop" },
 		p = { "<cmd>lua _PYTHON_TOGGLE()<cr>", "Python" },
 		l = { "<cmd>lua _LAZYGIT_TOGGLE()<cr>", "Lazygit" },
 		f = { "<cmd>ToggleTerm direction=float<cr>", "Float" },
 		h = { "<cmd>ToggleTerm size=10 direction=horizontal<cr>", "Horizontal" },
 		v = { "<cmd>ToggleTerm size=80 direction=vertical<cr>", "Vertical" },
+		t = { "<cmd>ToggleTerm<cr>", "toggle" },
 	},
 }
 
