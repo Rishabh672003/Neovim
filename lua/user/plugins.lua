c1d870a16863644f40ac6f965b0665e005dafee2
Rishabh <jharishabh2k3@gmail.com>
2022-05-22 18:39:58 +0530

    fixed formatting and added a markdown,css and many more language formatter prettier

--- lua/user/plugins.lua
+++ lua/user/plugins.lua
@@ -41,7 +41,7 @@ packer.init({
 -- plugins installed
 return packer.startup(function(use)
 	-- My plugins here
-	use("wbthomason/packer.nvim") -- Have packer manage itself
+	use({ "wbthomason/packer.nvim", auto_clean = true, auto_reload_compiled = true })
 	use("nvim-lua/popup.nvim") -- An implementation of the Popup API from vim in Neovim
 	use("nvim-lua/plenary.nvim") -- Useful lua functions used ny lots of plugins
 	use("windwp/nvim-autopairs") -- Autopairs, integrates with both cmp and treesitter
