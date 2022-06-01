ca55ceaa2db9012e950f411fa01d63eb4ecee10b
Rishabh <jharishabh2k3@gmail.com>
2022-06-01 23:28:03 +0530

    updated stuff

--- lua/user/lsp/configs.lua
+++ lua/user/lsp/configs.lua
@@ -5,7 +5,7 @@ end
 
 local lspconfig = require("lspconfig")
 
-local servers = { "sumneko_lua" , "bashls" }
+local servers = { "sumneko_lua" , "bashls", "ltex" }
 
 lsp_installer.setup {
 	ensure_installed = servers
