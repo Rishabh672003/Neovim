8a213161abcb4a3c06843fb04f5c9d8fe2af714e
Rishabh <jharishabh2k3@gmail.com>
2022-06-10 22:40:13 +0530

    corrected the formatting

--- lua/user/lsp/init.lua
+++ lua/user/lsp/init.lua
@@ -1,8 +1,8 @@
 local status_ok, _ = pcall(require, "lspconfig")
 if not status_ok then
-  return
+	return
 end
 
-require "user.lsp.configs"
+require("user.lsp.configs")
 require("user.lsp.handlers").setup()
-require "user.lsp.null-ls"
+require("user.lsp.null-ls")
