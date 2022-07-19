4fe1a3dd4d71cb41c5fe18d8a57c6ca5ae7ddfbd
Rishabh <jharishabh2k3@gmail.com>
2022-07-19 10:38:52 +0530

    updated the treesitter config

--- lua/user/treesitter.lua
+++ lua/user/treesitter.lua
@@ -9,7 +9,7 @@ if not status_ok1 then
 end
 
 configs.setup {
-  ensure_installed = { "lua"}, -- put the language you want in this array
+  ensure_installed = { "lua", "markdown"}, -- put the language you want in this array
   -- ensure_installed = "all",
   sync_install = false, -- install languages synchronously (only applied to `ensure_installed`)
   ignore_install = { }, -- List of parsers to ignore installing
