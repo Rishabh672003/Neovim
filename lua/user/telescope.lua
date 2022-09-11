7d18e87e319e62456384ec85ead0e22a1190c3de
Rishabh <jharishabh2k3@gmail.com>
2022-09-11 17:28:01 +0530

    update

--- lua/user/telescope.lua
+++ lua/user/telescope.lua
@@ -93,14 +93,14 @@ telescope.setup({
 			theme = "ivy",
 			-- disables netrw and use telescope-file-browser in its place
 			hijack_netrw = true,
-			mappings = {
-				["i"] = {
-					-- your custom insert mode mappings
-				},
-				["n"] = {
-					-- your custom normal mode mappings
-				},
-			},
+			-- mappings = {
+			-- 	["i"] = {
+			-- 		-- your custom insert mode mappings
+			-- 	},
+			-- 	["n"] = {
+			-- 		-- your custom normal mode mappings
+			-- 	},
+			-- },
 		},
 	},
 })
