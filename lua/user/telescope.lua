c2035b403c7056e769ecfe94f76bccfb936971db
Rishabh <jharishabh2k3@gmail.com>
2022-09-11 17:08:41 +0530

    updated the file browser extension of telescope

--- lua/user/telescope.lua
+++ lua/user/telescope.lua
@@ -88,5 +88,20 @@ telescope.setup({
 		-- Now the picker_config_key will be applied every time you call this
 		-- builtin picker
 	},
+	extensions = {
+		file_browser = {
+			theme = "ivy",
+			-- disables netrw and use telescope-file-browser in its place
+			hijack_netrw = true,
+			mappings = {
+				["i"] = {
+					-- your custom insert mode mappings
+				},
+				["n"] = {
+					-- your custom normal mode mappings
+				},
+			},
+		},
+	},
 })
-require("telescope").load_extension "file_browser"
+require("telescope").load_extension("file_browser")
