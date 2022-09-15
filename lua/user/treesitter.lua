7e9a8ae6bb1e9e2a138d148acbb78dd169969243
Rishabh <jharishabh2k3@gmail.com>
2022-09-15 23:29:38 +0530

    update

--- lua/user/treesitter.lua
+++ lua/user/treesitter.lua
@@ -1,29 +1,29 @@
 local status_ok, treesitter = pcall(require, "nvim-treesitter")
 if not status_ok then
-  return
+	return
 end
 
 local status_ok1, configs = pcall(require, "nvim-treesitter.configs")
 if not status_ok1 then
-  return
+	return
 end
 
 configs.setup({
-  ensure_installed = { "lua", "markdown", "markdown_inline" ,"bash", "python"}, -- put the language you want in this array
-  -- ensure_installed = "all",
-  sync_install = false, -- install languages synchronously (only applied to `ensure_installed`)
-  ignore_install = {"vim"}, -- List of parsers to ignore installing
-  autopairs = {
-    enable = true,
-  },
-  highlight = {
-    enable = true, -- false will disable the whole extension
-    disable = { "vim" }, -- list of language that will be disabled
-    additional_vim_regex_highlighting = true,
-  },
-  indent = { enable = true, disable = { "python", "css" } },
-  context_commentstring = {
-    enable = true,
-    enable_autocmd = false,
-  },
+	ensure_installed = { "lua", "markdown", "markdown_inline" ,"bash", "python"}, -- put the language you want in this array
+	-- ensure_installed = "all",
+	sync_install = false, -- install languages synchronously (only applied to `ensure_installed`)
+	ignore_install = { "vim" }, -- List of parsers to ignore installing
+	autopairs = {
+		enable = true,
+	},
+	highlight = {
+		enable = true, -- false will disable the whole extension
+		disable = { "vim" }, -- list of language that will be disabled
+		-- additional_vim_regex_highlighting = true,
+	},
+	indent = { enable = true, disable = { "python", "css" } },
+	context_commentstring = {
+		enable = true,
+		enable_autocmd = false,
+	},
 })
