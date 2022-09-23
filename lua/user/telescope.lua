a09d4e4ae63ef3de3d53e2ad7403fd8e66c0a071
Rishabh <jharishabh2k3@gmail.com>
2022-09-23 23:07:36 +0530

    updated the telescope config

--- lua/user/telescope.lua
+++ lua/user/telescope.lua
@@ -3,16 +3,71 @@ if not status_ok then
 	return
 end
 
-require("telescope").load_extension("file_browser")
-
 local actions = require("telescope.actions")
+
+local icons = require("user.icons")
+
+require("telescope").load_extension "file_browser"
 
 telescope.setup({
 	defaults = {
 
-		prompt_prefix = " ",
+		prompt_prefix = icons.ui.Telescope .. " ",
 		selection_caret = " ",
 		path_display = { "smart" },
+		file_ignore_patterns = {
+			".git/",
+			"target/",
+			"docs/",
+			"vendor/*",
+			"%.lock",
+			"__pycache__/*",
+			"%.sqlite3",
+			"%.ipynb",
+			"node_modules/*",
+			-- "%.jpg",
+			-- "%.jpeg",
+			-- "%.png",
+			"%.svg",
+			"%.otf",
+			"%.ttf",
+			"%.webp",
+			".dart_tool/",
+			".github/",
+			".gradle/",
+			".idea/",
+			".settings/",
+			".vscode/",
+			"__pycache__/",
+			"build/",
+			"env/",
+			"gradle/",
+			"node_modules/",
+			"%.pdb",
+			"%.dll",
+			"%.class",
+			"%.exe",
+			"%.cache",
+			"%.ico",
+			"%.pdf",
+			"%.dylib",
+			"%.jar",
+			"%.docx",
+			"%.met",
+			"smalljre_*/*",
+			".vale/",
+			"%.burp",
+			"%.mp4",
+			"%.mkv",
+			"%.rar",
+			"%.zip",
+			"%.7z",
+			"%.tar",
+			"%.bz2",
+			"%.epub",
+			"%.flac",
+			"%.tar.gz",
+		},
 
 		mappings = {
 			i = {
@@ -22,28 +77,33 @@ telescope.setup({
 				["<C-j>"] = actions.move_selection_next,
 				["<C-k>"] = actions.move_selection_previous,
 
+				["<C-b>"] = actions.results_scrolling_up,
+				["<C-f>"] = actions.results_scrolling_down,
+
 				["<C-c>"] = actions.close,
 
 				["<Down>"] = actions.move_selection_next,
 				["<Up>"] = actions.move_selection_previous,
 
 				["<CR>"] = actions.select_default,
-				["<C-x>"] = actions.select_horizontal,
+				["<C-s>"] = actions.select_horizontal,
 				["<C-v>"] = actions.select_vertical,
 				["<C-t>"] = actions.select_tab,
 
-				["<C-u>"] = actions.preview_scrolling_up,
-				["<C-d>"] = actions.preview_scrolling_down,
+				["<c-d>"] = require("telescope.actions").delete_buffer,
 
-				["<PageUp>"] = actions.results_scrolling_up,
-				["<PageDown>"] = actions.results_scrolling_down,
+				-- ["<C-u>"] = actions.preview_scrolling_up,
+				-- ["<C-d>"] = actions.preview_scrolling_down,
 
-				["<Tab>"] = actions.toggle_selection + actions.move_selection_worse,
-				["<S-Tab>"] = actions.toggle_selection + actions.move_selection_better,
+				-- ["<Tab>"] = actions.toggle_selection + actions.move_selection_worse,
+				["<Tab>"] = actions.close,
+				["<S-Tab>"] = actions.close,
+				-- ["<S-Tab>"] = actions.toggle_selection + actions.move_selection_better,
 				["<C-q>"] = actions.send_to_qflist + actions.open_qflist,
 				["<M-q>"] = actions.send_selected_to_qflist + actions.open_qflist,
 				["<C-l>"] = actions.complete_tag,
-				["<C-_>"] = actions.which_key, -- keys from pressing <C-/>
+				["<C-h>"] = actions.which_key, -- keys from pressing <C-h>
+				["<esc>"] = actions.close,
 			},
 
 			n = {
@@ -52,9 +112,13 @@ telescope.setup({
 				["<C-x>"] = actions.select_horizontal,
 				["<C-v>"] = actions.select_vertical,
 				["<C-t>"] = actions.select_tab,
+				["<C-b>"] = actions.results_scrolling_up,
+				["<C-f>"] = actions.results_scrolling_down,
 
-				["<Tab>"] = actions.toggle_selection + actions.move_selection_worse,
-				["<S-Tab>"] = actions.toggle_selection + actions.move_selection_better,
+				["<Tab>"] = actions.close,
+				["<S-Tab>"] = actions.close,
+				-- ["<Tab>"] = actions.toggle_selection + actions.move_selection_worse,
+				-- ["<S-Tab>"] = actions.toggle_selection + actions.move_selection_better,
 				["<C-q>"] = actions.send_to_qflist + actions.open_qflist,
 				["<M-q>"] = actions.send_selected_to_qflist + actions.open_qflist,
 
@@ -63,6 +127,11 @@ telescope.setup({
 				["H"] = actions.move_to_top,
 				["M"] = actions.move_to_middle,
 				["L"] = actions.move_to_bottom,
+				["q"] = actions.close,
+				["dd"] = require("telescope.actions").delete_buffer,
+				["s"] = actions.select_horizontal,
+				["v"] = actions.select_vertical,
+				["t"] = actions.select_tab,
 
 				["<Down>"] = actions.move_selection_next,
 				["<Up>"] = actions.move_selection_previous,
@@ -80,6 +149,7 @@ telescope.setup({
 		},
 	},
 	pickers = {
+
 		live_grep = {
 			theme = "dropdown",
 		},
@@ -118,6 +188,7 @@ telescope.setup({
 			theme = "dropdown",
 			initial_mode = "normal",
 		},
+
 		-- Default configuration for builtin pickers goes here:
 		-- picker_name = {
 		--   picker_config_key = value,
