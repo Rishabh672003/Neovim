70b2b453511328dda885084598e64a616d2bf4e3
Rishabh <jharishabh2k3@gmail.com>
2022-09-26 19:04:08 +0530

    removed useless code

--- lua/user/cmp.lua
+++ lua/user/cmp.lua
@@ -66,17 +66,6 @@ cmp.setup({
 		["<C-e>"] = cmp.mapping({
 			i = cmp.mapping.abort(),
 			c = cmp.mapping.close(),
-
-			--chris@machine's keymappings
-			--["<C-k>"] = cmp.mapping.select_prev_item(),
-			--["<C-j>"] = cmp.mapping.select_next_item(),
-			--["<C-b>"] = cmp.mapping(cmp.mapping.scroll_docs(-1), { "i", "c" }),
-			--["<C-f>"] = cmp.mapping(cmp.mapping.scroll_docs(1), { "i", "c" }),
-			--["<C-Space>"] = cmp.mapping(cmp.mapping.complete(), { "i", "c" }),
-			--["<C-y>"] = cmp.config.disable, -- Specify `cmp.config.disable` if you want to remove the default `<C-y>` mapping.
-			--["<C-e>"] = cmp.mapping {
-			--i = cmp.mapping.abort(),
-			--c = cmp.mapping.close(),
 		}),
 
 		-- Accept currently selected item. If none selected, `select` first item.
