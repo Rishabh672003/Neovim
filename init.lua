416b5bfac397854d840e8df05347712a3ef6694a
Rishabh <jharishabh2k3@gmail.com>
2022-10-08 13:52:15 +0530

    update

--- init.lua
+++ init.lua
@@ -31,7 +31,7 @@ local core_modules = {
 
 -- Using pcall we can handle better any loading issues
 for _, module in ipairs(core_modules) do
-	local ok, err = pcall(require, module)
+	local ok, _ = pcall(require, module)
 	if not ok then
 		print("config missing!!, or not loaded, see all config files are available and if they are restart neovim")
 		return
