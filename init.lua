0347d754e0c97be5c34ec5ff2dc8770298e60dc7
Rishabh <jharishabh2k3@gmail.com>
2022-10-15 22:25:47 +0530

    test didnt work

--- init.lua
+++ init.lua
@@ -1,7 +1,7 @@
 local core_modules = {
 	"user.impatient",
 	"user.plugins",
-	"user.colorschemes.catppuccin",
+	"user.colorschemes.tokyonight",
 	"user.lualine-themes.lualine1",
 	"user.startup-screens.startup-screen3",
 	"user.options",
