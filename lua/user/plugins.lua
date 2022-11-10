174e4e7c99be529d49b8b17e9b12481f71652042
Rishabh <jharishabh2k3@gmail.com>
2022-11-10 10:11:01 +0530

    update

--- lua/user/plugins.lua
+++ lua/user/plugins.lua
@@ -163,7 +163,7 @@ return packer.startup(function(use)
 
 	-- winbar stuff
 	use("SmiteshP/nvim-navic")
-	use("utilyre/barbecue.nvim")
+	use({ "utilyre/barbecue.nvim", branch = "issue/14" })
 
 	use("max397574/better-escape.nvim")
 	use({
@@ -173,6 +173,12 @@ return packer.startup(function(use)
 
 	use("folke/zen-mode.nvim")
 	use("ThePrimeagen/vim-be-good")
+
+	-- DAP
+	use({ "mfussenegger/nvim-dap", commit = "6b12294a57001d994022df8acbe2ef7327d30587" })
+	use({ "rcarriga/nvim-dap-ui", commit = "1cd4764221c91686dcf4d6b62d7a7b2d112e0b13" })
+	use({ "ravenxrz/DAPInstall.nvim", commit = "8798b4c36d33723e7bba6ed6e2c202f84bb300de" })
+	use({ "mfussenegger/nvim-dap-python", commit = "27a0eff2bd3114269bb010d895b179e667e712bd" })
 
 	-- graveyard of plugins
 
