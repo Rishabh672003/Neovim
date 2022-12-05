7f786ef03cf04d781469640c57ffad4c96fba5dd
Rishabh <jharishabh2k3@gmail.com>
2022-12-05 23:21:11 +0530

    update script

--- install.sh
+++ install.sh
@@ -1,7 +1,10 @@
 #!/bin/env bash
 
-sudo pacman -Sy --needed --noconfirm git base-devel cmake unzip ninja tree-sitter curl stylua prettier astyle ripgrep npm zsh autopep8 ; \
-    yay -S shellcheck-bin beautysh ;
+sudo pacman -Sy --needed --noconfirm git base-devel cmake unzip ninja \
+    tree-sitter curl lua &&\
+    sudo pacman -Sy -needed --noconfirm npm pip stylua prettier astyle \
+    ripgrep unzip npm zsh autopep8 ;
+yay -S shellcheck-bin beautysh ;
 git clone https://github.com/neovim/neovim &&
 cd neovim &&
 make CMAKE_BUILD_TYPE=RelWithDebInfo &&
