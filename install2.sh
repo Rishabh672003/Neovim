e7b24e0022dc36c674924ee2c9b4a2836a0d25f3
Rishabh <jharishabh2k3@gmail.com>
2023-02-09 00:14:59 +0530

    update

--- install2.sh
+++ install2.sh
@@ -1,12 +1,12 @@
 #!/bin/env bash
 
-sudo pacman -Sy --needed --noconfirm git base-devel cmake unzip ninja \
-    tree-sitter curl lua &&\
+sudo pacman -Sy --needed --noconfirm git base-devel unzip \
+    curl lua &&\
     sudo pacman -Sy --needed --noconfirm npm python-pip stylua prettier astyle \
     ripgrep unzip npm zsh autopep8 ;
 yay -S --needed --noconfirm shellcheck-bin beautysh ;
 rm -rf ~/nvim-linux64 > /dev/null 2>&1 ; \
-    curl --output-dir ~/neovim \
+    curl --output-dir ~/ \
     -LO https://github.com/neovim/neovim/releases/download/nightly/nvim-linux64.tar.gz && \
     tar -xvf ~/nvim-linux64.tar.gz;
 git clone https://github.com/Rishabh672003/Neovim ~/.config/nvim;
