13d91716ca8ae76bc55d914e54c52ae8af384e2d
Rishabh <jharishabh2k3@gmail.com>
2023-02-15 18:10:26 +0530

    update the script

--- install.sh
+++ install.sh
@@ -1,10 +1,14 @@
 #!/bin/env bash
 
 sudo pacman -Sy --needed --noconfirm git base-devel unzip \
-    curl lua &&\
+    curl lua cargo npm &&\
     sudo pacman -Sy --needed --noconfirm npm python-pip stylua prettier astyle \
-    ripgrep unzip npm zsh autopep8 ;
+    ripgrep unzip npm zsh autopep8 lua-language-server \
+    typescript-language-server bash-language-server pyright lemminx jdtls;
 yay -S --needed --noconfirm shellcheck-bin beautysh ;
+cargo install --features lsp --locked taplo-cli;
+cargo install prosemd-lsp;
+npm i -g vscode-langservers-extracted;
 rm -rf ~/nvim-linux64 > /dev/null 2>&1 ; \
     curl --output-dir ~ \
     -LO https://github.com/neovim/neovim/releases/download/nightly/nvim-linux64.tar.gz && \
@@ -16,3 +20,4 @@ echo "alias nvim='~/nvim-linux64/bin/nvim'" >> ~/.config/fish/config.fish ;
 source "$HOME/.bashrc";
 source "$HOME/.zshrc";
 nnv
+
