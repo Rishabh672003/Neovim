3aab7efed926b4e4f120760ff2f9de55bdebc567
Rishabh <jharishabh2k3@gmail.com>
2023-02-15 17:56:07 +0530

    update

--- install.sh
+++ install.sh
@@ -1,13 +1,18 @@
 #!/bin/env bash
 
-sudo pacman -Sy --needed --noconfirm git base-devel cmake unzip ninja \
-    tree-sitter curl lua &&\
+sudo pacman -Sy --needed --noconfirm git base-devel unzip \
+    curl lua &&\
     sudo pacman -Sy --needed --noconfirm npm python-pip stylua prettier astyle \
     ripgrep unzip npm zsh autopep8 ;
 yay -S --needed --noconfirm shellcheck-bin beautysh ;
-git clone https://github.com/neovim/neovim &&
-cd neovim &&
-make CMAKE_BUILD_TYPE=RelWithDebInfo &&
-sudo make install &&
-git clone https://github.com/Rishabh672003/Neovim ~/.config/nvim &&
-nvim
+rm -rf ~/nvim-linux64 > /dev/null 2>&1 ; \
+    curl --output-dir ~ \
+    -LO https://github.com/neovim/neovim/releases/download/nightly/nvim-linux64.tar.gz && \
+    tar -xvf ~/nvim-linux64.tar.gz;
+git clone https://github.com/Rishabh672003/Neovim ~/.config/nvim;
+echo "alias nvim='~/nvim-linux64/bin/nvim'" >> ~/.bashrc ;
+echo "alias nvim='~/nvim-linux64/bin/nvim'" >> ~/.zshrc ;
+echo "alias nvim='~/nvim-linux64/bin/nvim'" >> ~/.config/fish/config.fish ;
+source "$HOME/.bashrc";
+source "$HOME/.zshrc";
+nnv
