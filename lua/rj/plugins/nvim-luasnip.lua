77b740a49379a04405e7b08015fe8482eab3c541
Rishabh <jharishabh2k3@gmail.com>
2023-03-26 14:47:50 +0530

    update: java boilerplate

--- lua/rj/plugins/nvim-luasnip.lua
+++ lua/rj/plugins/nvim-luasnip.lua
@@ -63,7 +63,6 @@ int main() {{
 	),
 })
 
-
 ls.add_snippets("cpp", {
 	s(
 		"cc",
@@ -128,13 +127,66 @@ ls.add_snippets("java", {
 		"cc",
 		fmt(
 			[[
+import java.io.*;
 import java.util.*;
 
-class Test {{
-    public static void main(String args[]){{
-        Scanner sc = new Scanner(System.in);
-		{}
+public class Test {{
+  static PrintWriter out=new PrintWriter((System.out));
+  static Reader sc=new Reader();
+  public static void main(String args[])throws IOException {{
+    int t=sc.nextInt();
+    while(t-->0) {{
+      solve();
     }}
+      out.close();
+  }}
+
+  public static void solve() {{
+		{}
+  }}
+    
+  static class Reader {{ 
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    StringTokenizer st = new StringTokenizer("");
+    public String next() {{
+      while(!st.hasMoreTokens()) {{
+        try {{
+          st = new StringTokenizer(br.readLine());
+        }} catch(Exception e) {{
+          e.printStackTrace();
+        }}
+      }}
+      return st.nextToken();
+    }}
+    public int nextInt() {{
+      return Integer.parseInt(next());
+    }}
+    public long nextLong() {{
+      return Long.parseLong(next());
+    }}
+    public double nextDouble() {{
+      return Double.parseDouble(next());
+    }}
+    public String nextLine() {{
+      try {{
+        return br.readLine();
+      }} catch(Exception e) {{
+        e.printStackTrace();
+      }}
+      return null;
+    }}
+    public boolean hasNext() {{
+      String next = null;
+      try {{
+        next = br.readLine();
+      }} catch(Exception e) {{}}
+      if(next == null) {{
+        return false;
+      }}
+      st = new StringTokenizer(next);
+      return true;
+    }}
+  }}
 }}
 ]],
 			{
