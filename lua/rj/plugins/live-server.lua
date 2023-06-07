d1e2e8bbc67c8d9f3a7dbe4efc9f906e312b735e
Rishabh <jharishabh2k3@gmail.com>
2023-06-07 16:41:28 +0530

    added live-server.nvim

---
+++ lua/rj/plugins/live-server.lua
@@ -0,0 +1,12 @@
+local M = {
+	"barrett-ruth/live-server.nvim",
+	ft = { "javascript", "javascriptreact", "typescript", "typescriptreact" },
+	build = "yarn global add live-server",
+	config = true,
+}
+
+function M.config()
+	require("live-server").setup()
+end
+
+return M
