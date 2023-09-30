ef7c8428b0837f3551a943be332cf61eb4bd14f9
Rishabh <53911515+Rishabh672003@users.noreply.github.com>
2023-09-30 17:09:23 +0530

    updated the lspconfig for clangd

--- ftplugin/java.lua
+++ ftplugin/java.lua
@@ -86,8 +86,8 @@ local config = {
     workspace_dir,
   },
 
-  on_attach = require("user.lsp.handlers").on_attach,
-  capabilities = require("user.lsp.handlers").capabilities,
+  on_attach = require("rj.lsp.attach").on_attach,
+  capabilities = require("rj.lsp.attach").capabilities,
 
   -- 💀
   -- This is the default if not provided, you can remove it. Or adjust as needed.
