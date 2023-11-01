523adf8d3d5463abc9c1f4e153b8982f6361d49a
Rishabh <53911515+Rishabh672003@users.noreply.github.com>
2023-11-01 20:14:08 +0530

    fix(autocommands): pressing q will quit oil.nvim windows

--- lua/rj/autocommands.lua
+++ lua/rj/autocommands.lua
@@ -60,9 +60,11 @@ vim.api.nvim_create_autocmd({ "BufWinEnter" }, {
 })
 
 vim.api.nvim_create_autocmd({ "FileType" }, {
-  pattern = { "neo-tree" },
+  pattern = { "neo-tree", "oil" },
   callback = function()
     vim.opt_local.number = false
+    vim.opt_local.relativenumber = false
+    vim.keymap.set("n", "q", "<cmd>q<CR>", { silent = true })
   end,
 })
 
@@ -76,7 +78,7 @@ vim.api.nvim_create_autocmd({ "BufReadPost" }, {
 })
 
 vim.api.nvim_create_autocmd({ "FileType" }, {
-  pattern = { "c", "go", "java", "cpp" },
+  pattern = { "c", "go", "java", "cpp", "h", "hpp" },
   callback = function()
     vim.opt.shiftwidth = 4
     vim.opt.tabstop = 4
@@ -88,6 +90,19 @@ vim.filetype.add({
     rasi = "rasi",
     conf = "conf",
   },
+})
+
+vim.api.nvim_create_autocmd("ModeChanged", {
+  pattern = "*",
+  callback = function()
+    if
+      ((vim.v.event.old_mode == "s" and vim.v.event.new_mode == "n") or vim.v.event.old_mode == "i")
+      and require("luasnip").session.current_nodes[vim.api.nvim_get_current_buf()]
+      and not require("luasnip").session.jump_active
+    then
+      require("luasnip").unlink_current()
+    end
+  end,
 })
 
 -- make a command to clear registers
