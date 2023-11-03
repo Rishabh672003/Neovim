3aca21be33920d693c3e7c0f48ac76be909ee927
Rishabh <53911515+Rishabh672003@users.noreply.github.com>
2023-11-03 23:05:53 +0530

    refactor(lsp): removed unecessary comment

--- lua/rj/plugins/lsp.lua
+++ lua/rj/plugins/lsp.lua
@@ -42,11 +42,7 @@ return {
     "folke/trouble.nvim",
     dependencies = { "nvim-tree/nvim-web-devicons" },
     event = "VeryLazy",
-    opts = {
-      -- your configuration comes here
-      -- or leave it empty to use the default settings
-      -- refer to the configuration section below
-    },
+    opts = {},
   },
   {
     "p00f/clangd_extensions.nvim",
