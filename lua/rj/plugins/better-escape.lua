7ef30320a7321c3217f88bea15a762218540d1c6
Rishabh <53911515+Rishabh672003@users.noreply.github.com>
2023-11-09 20:20:51 +0530

    feat(better-escape): removed the event and added the keys instead

--- lua/rj/plugins/better-escape.lua
+++ lua/rj/plugins/better-escape.lua
@@ -1,6 +1,6 @@
 local M = {
   "max397574/better-escape.nvim",
-  event = "BufRead",
+  keys = { { "jk", mode = "i" } },
 }
 
 function M.config()
@@ -10,7 +10,7 @@ function M.config()
     }, -- a table with mappings to use
     timeout = vim.o.timeoutlen, -- the time in which the keys must be hit in ms. Use option timeoutlen by default
     clear_empty_lines = false, -- clear line after escaping if there is only whitespace
-    keys = "<Esc>", -- keys used for escaping, if it is a function will use the result everytime
+    keys = "<Esc>", -- keys used for escaping, if it is a function will use the result every time
     -- example(recommended)
     -- keys = function()
     --   return vim.api.nvim_win_get_cursor(0)[2] > 1 and '<esc>l' or '<esc>'
