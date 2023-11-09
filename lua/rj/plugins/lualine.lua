2a7eabe1de024367b33558ea448ad779d5b08e2d
Rishabh <53911515+Rishabh672003@users.noreply.github.com>
2023-11-09 20:25:21 +0530

    feat(lualine): updated the deprecated function

--- lua/rj/plugins/lualine.lua
+++ lua/rj/plugins/lualine.lua
@@ -62,7 +62,7 @@ function M.config()
   end
 
   local spaces = function()
-    return "spaces: " .. vim.api.nvim_buf_get_option(0, "shiftwidth")
+    return "spaces: " .. vim.api.nvim_get_option_value("shiftwidth", { buf = 0 })
   end
 
   lualine.setup({
