07ef22835b5001b5a530e8eb4886a17000171c2b
Rishabh <53911515+Rishabh672003@users.noreply.github.com>
2023-11-18 23:09:01 +0530

    fix(whichkey): disable marks

--- lua/rj/plugins/whichkey.lua
+++ lua/rj/plugins/whichkey.lua
@@ -8,8 +8,8 @@ function M.config()
   local which_key = require("which-key")
   local setup = {
     plugins = {
-      marks = true, -- shows a list of your marks on ' and `
-      registers = true, -- shows your registers on " in NORMAL or <C-r> in INSERT mode
+      marks = false, -- shows a list of your marks on ' and `
+      registers = false, -- shows your registers on " in NORMAL or <C-r> in INSERT mode
       spelling = {
         enabled = true, -- enabling this will show WhichKey when pressing z= to select spelling suggestions
         suggestions = 20, -- how many suggestions should be shown in the list?
