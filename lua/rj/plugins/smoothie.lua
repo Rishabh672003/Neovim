a6409c28517ff5af0c3ed25020ef9f593cf7df0d
Rishabh <53911515+Rishabh672003@users.noreply.github.com>
2023-11-23 20:42:37 +0530

    fix(vim-smoothie): removed the unnecessary keybind

--- lua/rj/plugins/smoothie.lua
+++ lua/rj/plugins/smoothie.lua
@@ -6,14 +6,6 @@ local M = {
 
 vim.g.smoothie_hide_cursor = 1
 
-vim.cmd([[
-  nnoremap <C-j> <cmd>call smoothie#do("\<C-D>") <CR>
-  vnoremap <C-j> <cmd>call smoothie#do("\<C-D>") <CR>
-
-  nnoremap <C-k> <cmd>call smoothie#do("\<C-U>") <CR>
-  vnoremap <C-k> <cmd>call smoothie#do("\<C-U>") <CR>
-]])
-
 vim.g.smoothie_remapped_commands = {
   "<C-D>",
   "<C-U>",
