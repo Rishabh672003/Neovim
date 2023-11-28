1f64ca0bf37dfb3e92dbeacd88d9fc5d80c3cc90
Rishabh <53911515+Rishabh672003@users.noreply.github.com>
2023-11-28 16:36:36 +0530

    fix(guifont-options): fixed the font name

--- lua/rj/options.lua
+++ lua/rj/options.lua
@@ -35,7 +35,7 @@ local options = {
   virtualedit = "onemore",
   linebreak = true,
   showtabline = 1,
-  guifont = "JetBrainsMono Nerd Font Regular:h14", -- the font used in graphical neovim applications
+  guifont = "JetBrainsMono NF Regular:h14", -- the font used in graphical neovim applications
   spelllang = "en_us",
   textwidth = 80,
 }
