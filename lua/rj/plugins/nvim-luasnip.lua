dd62b6c34e69847ae89e1b5c0947fc311c1c3ba5
Rishabh <53911515+Rishabh672003@users.noreply.github.com>
2023-12-03 16:03:50 +0530

    feat(luasnip): add an aoc snippet

--- lua/rj/plugins/nvim-luasnip.lua
+++ lua/rj/plugins/nvim-luasnip.lua
@@ -109,6 +109,46 @@ int main() {{
 
   ls.add_snippets("cpp", {
     s(
+      "aoc",
+      fmt(
+        [[
+#include <fstream>
+#include <functional>
+#include <iostream>
+#include <string>
+
+using std::cout, std::cin, std::endl, std::string;
+
+int main() {{
+    std::ifstream myfile("input.txt");
+
+    if (!myfile) {{
+        std::cerr << "Input file cannot be opened for reading.\n";
+
+        return 1;
+    }}
+
+    while (myfile) {{
+        string line;
+        myfile >> line;
+        if (line.empty()) {{
+            break;
+        }}
+        {}
+    }}
+    myfile.close();
+    return 0;
+}}
+]],
+        {
+          i(1),
+        }
+      )
+    ),
+  })
+
+  ls.add_snippets("cpp", {
+    s(
       "ccc",
       fmt(
         [[
