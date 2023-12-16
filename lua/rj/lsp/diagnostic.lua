bfe50cc3a57d1ad8158666e9dad30b49967f72cf
Rishabh <53911515+Rishabh672003@users.noreply.github.com>
2023-12-16 23:14:24 +0530

    feat!(deagnostic): updated signs for nvim-0.10

--- lua/rj/lsp/diagnostic.lua
+++ lua/rj/lsp/diagnostic.lua
@@ -1,20 +1,12 @@
-local signs = {
-  { name = "DiagnosticSignError", text = "" },
-  { name = "DiagnosticSignWarn", text = "" },
-  { name = "DiagnosticSignHint", text = "" },
-  { name = "DiagnosticSignInfo", text = "" },
-}
-
-for _, sign in ipairs(signs) do
-  vim.fn.sign_define(sign.name, { texthl = sign.name, text = sign.text, numhl = "" })
-end
-
 local config = {
-  -- disable virtual text
   virtual_text = false,
-  -- show signs
   signs = {
-    active = signs,
+    text = {
+      [vim.diagnostic.severity.ERROR] = "",
+      [vim.diagnostic.severity.WARN] = "",
+      [vim.diagnostic.severity.HINT] = "",
+      [vim.diagnostic.severity.INFO] = "",
+    },
   },
   update_in_insert = true,
   underline = true,
