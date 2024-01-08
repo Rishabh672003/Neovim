162947ea14947f9b961d729c6c11a6f8a8ffeeee
Rishabh <53911515+Rishabh672003@users.noreply.github.com>
2024-01-08 16:35:44 +0530

    refactor(mason): mason tools installer is the main plugin

--- lua/rj/plugins/mason.lua
+++ lua/rj/plugins/mason.lua
@@ -1,66 +1,64 @@
 local M = {
-  "williamboman/mason.nvim",
-  lazy = true,
-  event = { "BufReadPost", "BufNewFile", "BufWritePre" },
+  "WhoIsSethDaniel/mason-tool-installer.nvim",
   dependencies = {
+    "williamboman/mason-lspconfig.nvim",
     {
-      "williamboman/mason-lspconfig.nvim",
-    },
-    {
-      "WhoIsSethDaniel/mason-tool-installer.nvim",
-      config = function()
-        require("mason-tool-installer").setup({
-          ensure_installed = {
-            -- language servers
-            "bashls",
-            "cssls",
-            "dockerls",
-            "gopls",
-            "jdtls",
-            "jsonls",
-            "lemminx",
-            "lua_ls",
-            "marksman",
-            "pyright",
-            "tailwindcss",
-            "taplo",
-            "tsserver",
-            "yamlls",
-            "cmake",
-
-            -- debug adapters
-            "codelldb",
-
-            -- formatters
-            -- astyle missing
-            "biome",
-            "prettier",
-            "black",
-            "shfmt",
-            "stylua",
-            "cmakelang",
-
-            -- linters
-            -- cppcheck missing
-            "eslint_d",
-            "luacheck",
-            "proselint",
-            "shellcheck",
-          },
-        })
-      end,
+      "williamboman/mason.nvim",
+      require("mason").setup({
+        ui = {
+          border = "rounded",
+        },
+        registries = {
+          "github:nvim-java/mason-registry",
+          "github:mason-org/mason-registry",
+        },
+      }),
     },
   },
+  lazy = true,
 }
 
 function M.config()
-  require("mason").setup({
-    ui = {
-      border = "rounded",
-    },
-    registries = {
-      "github:nvim-java/mason-registry",
-      "github:mason-org/mason-registry",
+  require("mason-tool-installer").setup({
+    ensure_installed = {
+      -- language servers
+      "bashls",
+      "clangd",
+      "cmake",
+      "cssls",
+      "dockerls",
+      "gopls",
+      "jdtls",
+      "jsonls",
+      "lemminx",
+      "lua_ls",
+      "marksman",
+      "pyright",
+      "tailwindcss",
+      "taplo",
+      "tsserver",
+      "yamlls",
+
+      -- debug adapters
+      "codelldb",
+
+      -- formatters
+      -- astyle missing
+      "biome",
+      "black",
+      "clang-format",
+      "cmakelang",
+      "codespell",
+      "prettier",
+      "shfmt",
+      "stylua",
+
+      -- linters
+      -- cppcheck missing
+      "eslint_d",
+      "luacheck",
+      "proselint",
+      "shellcheck",
     },
   })
 end
