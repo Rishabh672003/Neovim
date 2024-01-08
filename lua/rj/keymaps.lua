2a3f2e8db276120e1b25857b7101f8d3e913203e
Rishabh <53911515+Rishabh672003@users.noreply.github.com>
2024-01-08 12:48:26 +0530

    feat(keymaps): change from `-` to `=` in keymap

--- lua/rj/keymaps.lua
+++ lua/rj/keymaps.lua
@@ -57,10 +57,10 @@ keymap("n", "dc", "cc<esc>", opts)
 
 -- Visual Block --
 -- Move text up and down
-keymap("x", "J", ":move '>+1<CR>gv-gv", opts)
-keymap("x", "K", ":move '<-2<CR>gv-gv", opts)
-keymap("x", "<A-j>", ":move '>+1<CR>gv-gv", opts)
-keymap("x", "<A-k>", ":move '<-2<CR>gv-gv", opts)
+keymap("x", "J", ":move '>+1<CR>gv=gv", opts)
+keymap("x", "K", ":move '<-2<CR>gv=gv", opts)
+keymap("x", "<A-j>", ":move '>+1<CR>gv=gv", opts)
+keymap("x", "<A-k>", ":move '<-2<CR>gv=gv", opts)
 
 -- Terminal --
 -- Better terminal navigation
