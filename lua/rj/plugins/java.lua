56e241aed0ea5dbc73f60fb2f9d932142b113b30
Rishabh <53911515+Rishabh672003@users.noreply.github.com>
2024-01-12 22:47:47 +0530

    refactor(nvim-java): changed structure

--- lua/rj/plugins/java.lua
+++ lua/rj/plugins/java.lua
@@ -1,22 +1,24 @@
-return {
+local M = {
   "nvim-java/nvim-java",
   ft = "java",
-  config = function()
-    require("java").setup()
-    require("lspconfig").jdtls.setup({})
-  end,
-  dependencies = {
-    "nvim-java/lua-async-await",
-    "nvim-java/nvim-java-core",
-    "nvim-java/nvim-java-test",
-    "nvim-java/nvim-java-dap",
-    "MunifTanjim/nui.nvim",
-    {
-      "neovim/nvim-lspconfig",
-    },
-    "mfussenegger/nvim-dap",
-    {
-      "williamboman/mason.nvim",
-    },
-  },
 }
+
+M.dependencies = {
+  "neovim/nvim-lspconfig",
+  "williamboman/mason.nvim",
+  "nvim-java/lua-async-await",
+  "nvim-java/nvim-java-core",
+  "nvim-java/nvim-java-test",
+  "nvim-java/nvim-java-dap",
+  "MunifTanjim/nui.nvim",
+  "mfussenegger/nvim-dap",
+}
+
+function M.config()
+  require("java").setup()
+  require("lspconfig").jdtls.setup({})
+  local opts = { noremap = true, silent = true, buffer = true }
+  vim.keymap.set("n", "gl", "<cmd>lua vim.diagnostic.open_float()<CR>", opts)
+end
+
+return M
