50afa4c5970d194d144d2358d1c5110907dc1e66
Rishabh <53911515+Rishabh672003@users.noreply.github.com>
2024-06-22 18:15:36 +0530

    feat(python): added autoimport capabilities using autoimport

---
+++ ftplugin/python.lua
@@ -0,0 +1,41 @@
+local status_ok, _ = pcall(require, "swenv")
+if not status_ok then
+  return "Swenv not found"
+else
+  require("swenv.api").auto_venv()
+end
+
+local status, wk = pcall(require, "which-key")
+if not status then
+  return "Whick-key not found"
+end
+
+local keymaps = {
+  ["<leader>lpo"] = {
+    function()
+      local function on_exit(job_id, data, event)
+        vim.cmd("e")
+        vim.notify("AutoImport completed")
+      end
+
+      local function on_output(job_id, data, event)
+      end
+
+      -- Start the job asynchronously
+      vim.fn.jobstart("autoimport " .. vim.fn.expand("%"), {
+        on_exit = on_exit,
+        on_stdout = on_output,
+        on_stderr = on_output,
+      })
+    end,
+    "Import Symbol",
+  },
+  ["<leader>lps"] = {
+    function()
+      require("swenv.api").pick_venv()
+    end,
+    "Pick venv",
+  },
+}
+
+wk.register(keymaps)
