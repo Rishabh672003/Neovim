eafca6d12ad4f72b79e94a163c68b6d6c1d28e73
Rishabh <53911515+Rishabh672003@users.noreply.github.com>
2024-10-15 19:56:09 +0530

    refactor: moved from ftplugin to here

--- lua/rj/autocommands.lua
+++ lua/rj/autocommands.lua
@@ -6,15 +6,15 @@ vim.api.nvim_create_autocmd("FileType", {
 })
 
 autocmd({ "FileType" }, {
-  pattern = { "qf", "help", "lspinfo", "spectre_panel", "oil", "Jaq" },
+  pattern = { "qf", "help", "lspinfo", "spectre_panel", "oil", "Jaq", "man" },
   callback = function()
-    vim.keymap.set("n", "q", "<cmd>close<CR>", { silent = true, buffer = true })
+    vim.keymap.set("n", "q", "<cmd>close!<CR>", { silent = true, buffer = true })
     vim.api.nvim_set_option_value("buflisted", false, { buf = 0 })
   end,
 })
 
 vim.api.nvim_create_autocmd("FileType", {
-  pattern = { "go", "c", "cpp", "rust", "java" },
+  pattern = { "go", "c", "cpp", "rust", "java", "js" },
   command = "setlocal tabstop=4 shiftwidth=4",
 })
 
@@ -23,29 +23,12 @@ autocmd({ "FileType" }, {
   callback = function()
     vim.opt_local.wrap = true
     vim.opt_local.spell = true
+    vim.opt.textwidth = 120
   end,
 })
-
--- make a command to clear registers
-vim.cmd([[
-command! WipeReg for i in range(34,122) | silent! call setreg(nr2char(i), []) | endfor
-]])
 
 vim.api.nvim_create_user_command("Grep", function(opts)
   local command = string.format('silent cgetexpr system("rg --vimgrep -S %s")', opts.args)
   vim.cmd(command)
   vim.cmd("copen")
 end, { nargs = 1 })
-
--- Define an autocommand after LspAttach
-vim.api.nvim_create_autocmd("LspAttach", {
-  callback = function(args)
-    local bufnr = args.buf
-    local filetype = vim.api.nvim_get_option_value("filetype", { buf = bufnr })
-    if filetype == "rust" or filetype == "go" then
-      vim.lsp.inlay_hint.enable(false)
-    else
-      vim.lsp.inlay_hint.enable(true)
-    end
-  end,
-})
