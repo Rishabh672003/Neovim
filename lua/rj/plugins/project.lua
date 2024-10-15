00dbd2d09f6f67fdb5f3f0ca957a6cd5980f0af6
Rishabh <53911515+Rishabh672003@users.noreply.github.com>
2024-10-15 20:07:58 +0530

    fix: dont notify everytime it changes cwd

--- lua/rj/plugins/project.lua
+++ lua/rj/plugins/project.lua
@@ -11,7 +11,7 @@ Later(function()
     ---@usage patterns used to detect root dir, when **"pattern"** is in detection_methods
     patterns = { ".git", "_darcs", ".hg", ".bzr", ".svn", "Makefile", "package.json", "LICENSE", ".venv", "go.mod" },
     show_hidden = false,
-    silent_chdir = false,
+    silent_chdir = true,
     ---@usage list of lsp client names to ignore when using **lsp** detection. eg: { "efm", ... }
     ignore_lsp = {},
     ---@type string
