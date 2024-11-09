d8115da6218c855541e8a52e838db2b0882af5e5
Rishabh <53911515+Rishabh672003@users.noreply.github.com>
2024-11-09 17:06:33 +0530

    feat: switch to python scripts

---
+++ utils/install.py
@@ -0,0 +1,61 @@
+# Maintainer: Rishabh672003
+# only supported for ArchLinux and Windows
+
+import os
+import platform
+import shutil
+import subprocess
+from pathlib import Path
+
+# fmt: off
+neovim_linux_config = Path.home() / ".config" / "nvim"
+neovim_windows_config = Path.home() / "AppData" / "Local" / "nvim"
+repo_link = "https://github.com/Rishabh672003/Neovim"
+mason_dependencies = [ "git", "curl", "unzip", "tar", "gzip", "luarocks", "npm", "python-pip", "yarn", "go" ]
+# fmt: on
+
+
+def arch_linux_setup():
+    # get the neovim and the dependencies
+    if shutil.which("pacman"):
+        # fmt: off
+        subprocess.run(["sudo", "pacman", "-S", "--needed", "--noconfirm", "neovim" ])
+        subprocess.run(["sudo", "pacman", "-S", "--needed", "--noconfirm", *mason_dependencies ])
+        # fmt: on
+    # make the dir if it doesnt exit
+    if not os.path.isdir(neovim_linux_config):
+        os.makedirs(neovim_linux_config, exist_ok=True)
+        print(f"Created directory: {neovim_linux_config}")
+    else:
+        pass
+
+    # if already a directory exists and not not empty take a backup
+    if os.listdir(neovim_linux_config):
+        backup_path = f"{neovim_linux_config}.backup"
+        shutil.move(neovim_linux_config, backup_path)
+        print(f"Directory was not empty. Backed up to: {backup_path}")
+
+        # Recreate the empty nvim directory for cloning
+        os.makedirs(neovim_linux_config, exist_ok=True)
+    else:
+        print("Directory is empty. No backup needed.")
+
+    # finally clone the rep in the directory
+    subprocess.run(["git", "clone", repo_link, neovim_linux_config])
+
+
+def windown_setup():
+    if not shutil.which("winget"):
+        return Exception("Winget not found, please install winget")
+    else:
+        subprocess.run(["winget", "install", "Neovim.Neovim"])
+        if not shutil.which("git"):
+            subprocess.run(["winget", "install", "Git.Git"])
+        subprocess.run(["git", "clone", repo_link, neovim_windows_config])
+
+
+if __name__ == "__main__":
+    if platform.system() == "Windows":
+        windown_setup()
+    else:
+        arch_linux_setup()
---
+++ utils/startuptime.py
@@ -0,0 +1,49 @@
+import time
+from subprocess import DEVNULL, run
+
+startup_log_file: str = "tmp_" + str(time.time()).replace(".", "_")
+
+
+def get_time(filepath: str = startup_log_file):
+    # we want the second line that contains nvim started
+    # TODO: is there a better way to get this
+    found = False
+    with open(filepath, "r") as file:
+        for i in file.readlines():
+            if "NVIM STARTED" in i:
+                time = i.split()[0]
+                if found:
+                    return time
+                found = True
+
+
+time = lambda: get_time()
+
+for i in range(5):
+    print(f"Warmup #{i+1}")
+    run(["nvim", "-c", "q"], stdout=DEVNULL, stderr=DEVNULL)
+
+
+run(["nvim", "--clean", "-nu", "NORC", "--startuptime", startup_log_file])
+print(f"No config: {time()}ms")
+run(["rm", startup_log_file])
+
+run(["nvim", "--startuptime", startup_log_file])
+print(f"With config: {time()}ms")
+run(["rm", startup_log_file])
+
+run(["nvim", "tmp.lua", "--startuptime", startup_log_file])
+print(f"Opening init.lua: {time()}ms")
+run(["rm", startup_log_file])
+
+run(["nvim", "tmp.py", "--startuptime", startup_log_file])
+print(f"Opening Python file: {time()}ms")
+run(["rm", startup_log_file])
+
+run(["nvim", "tmp.cpp", "--startuptime", startup_log_file])
+print(f"Opening CPP file: {time()}ms")
+run(["rm", startup_log_file])
+
+run(["nvim", "tmp.go", "--startuptime", startup_log_file])
+print(f"Opening Go file: {time()}ms")
+run(["rm", startup_log_file])
