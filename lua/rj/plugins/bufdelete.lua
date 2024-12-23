6ae0dd1edc586babf7903ffb6d80fc7ff8bd3e4e
Rishabh <53911515+Rishabh672003@users.noreply.github.com>
2024-12-23 23:07:29 +0530

    fix: remove not used plugin files

--- lua/rj/plugins/bufdelete.lua
+++
@@ -1,7 +0,0 @@
-Later(function()
-  Add({
-    source = "famiu/bufdelete.nvim",
-  })
-
-  vim.keymap.set("n", "<Leader>c", "<Cmd>Bdelete!<CR>", { silent = true, desc = "Close the buffer" })
-end)
--- lua/rj/plugins/hardtime.lua
+++
@@ -1,7 +0,0 @@
-Later(function ()
-  Add({
-    source = "m4xshen/hardtime.nvim",
-    depends = { "MunifTanjim/nui.nvim", "nvim-lua/plenary.nvim" }
-  })
-  require("hardtime").setup()
-end)
